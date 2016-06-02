0b31b3e0677f46adb186ea12026602e61ada8815
Krista Moroder <kmoroder@gmail.com>
2016-06-02 14:59:51 -0700

    update intro

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,10 @@ export default class Introduction extends Component {
     return (
       <div className="introduction-container">
         <div className="introduction">
-          <h4>I am a full stack software engineer in San Francisco. I enjoy working across the full stack to solve tough engineering challenges, and I have been extra excited lately about database optimization and efficient state management in single-page apps. Most recently, I have been working a lot with JavaScript (ES6) with React / Redux, Node.js, and both SQL and NoSQL databases (particularly PostgreSQL, Redis, and MongoDB).</h4>
+          <h4>
+            <p>I am a software engineer in San Francisco. I enjoy solving tough engineering challenges, and I've been particularly interested lately in database optimization and efficient state management in single-page apps.</p>
+            <p>As an example of some of my work, I recently built an IoT application that homeowners (or AirBnB hosts!) use to rent appliances to guests. To solve the challenge of managing the hardware integration, I automated API interactions with a worker I built using Node.js, Cron, and Redis. I also optimized data management across the application by implementing transactions with PostgreSQL on the back end and isolating the state on the front end with React / Redux. You can read more about that application, and others, below.</p>
+          </h4>
           <h5>
             <p><strong>Three things:</strong></p>
             <p>1.  A year and a half ago, I had never been on a sailboat. I now have my Captain license.</p>
