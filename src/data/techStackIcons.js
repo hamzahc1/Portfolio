4c0feb6b58960c1f6a02a5fb825231e62c4c061a
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-06-07 20:05:05 +0100

    Update tech stack icons

--- src/data/techStackIcons.js
+++ src/data/techStackIcons.js
@@ -22,6 +22,10 @@ const iconsData = {
   AWS: 'https://assets.toptal.io/uploads/blog/category/logo/106/C.png',
   'Digital Ocean': 'https://static.blockspring.com/tag-icons/digital%20ocean.png',
   Bitrise: 'https://pbs.twimg.com/profile_images/509277348965781504/VRskGFs5.jpeg',
+  MySQL: 'https://www.mysql.com/common/logos/logo-mysql-170x115.png',
+  Sequelize: 'https://milinaudara.files.wordpress.com/2014/05/logo.png',
+  Facebook Developer: 'https://cdn.churchm.ag/wp-content/uploads/2011/10/Facebook-developers-logo.png',
+
 };
 
 export { iconsData };
