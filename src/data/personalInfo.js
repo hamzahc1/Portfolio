a67e6c679013d086f6f3f1abaac144d4ab740ba0
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-06-07 19:40:46 +0100

    Update personal data

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -1,9 +1,9 @@
 const personalInfoData = {
-  name: 'Krista Moroder',
-  linkedIn: 'https://www.linkedin.com/in/kmoroder',
-  github: 'https://github.com/kmoroder',
-  angelList: 'https://angel.co/kmoroder',
-  email: 'mailto:kmoroder@gmail.com',
+  name: 'Hamzah Chaudhary',
+  linkedIn: 'https://www.linkedin.com/in/hamzahc',
+  github: 'https://github.com/hamzahc1',
+  angelList: 'https://angel.co/hamzah-chaudhary',
+  email: 'mailto:hamzahchaudhary@gmail.com',
   resume: 'https://www.dropbox.com/s/5vzlsibsjoj347y/ResumeKristaMoroderEngineer.pdf?dl=0'
 };
 
