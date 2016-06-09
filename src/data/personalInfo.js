521406b1c236418d6a156c97b922448b975b5c02
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-06-09 23:35:19 +0100

    Update resume file

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -4,7 +4,7 @@ const personalInfoData = {
   github: 'https://github.com/hamzahc1',
   angelList: 'https://angel.co/hamzah-chaudhary',
   email: 'mailto:hamzahchaudhary@gmail.com',
-  resume: 'https://www.dropbox.com/s/u7kqf69leomqgoh/HamzahChaudharyResume.pdf?dl=0'
+  resume: 'https://www.dropbox.com/s/6v9vl7wy429es8s/HamzahChaudharyResume.pdf?dl=0'
 };
 
 export { personalInfoData };
